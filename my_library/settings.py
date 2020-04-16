5cbd8fc02310c9658d6a03d1c2029481ca079742
Natthaphol Aphaiso <natthaphol.dev@gmail.com>
2020-04-16 22:06:12 +0700

    no message

--- my_library/settings.py
+++ my_library/settings.py
@@ -120,5 +120,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT='/var/www/staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, "/var/www/staticfiles/")
 STATIC_URL = '/static/'

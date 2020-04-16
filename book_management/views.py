359ddf93736f2d34a1c380bb571bc93f32f02858
Natthaphol Aphaiso <natthaphol.dev@gmail.com>
2020-04-16 21:13:46 +0700

    Init.

---
+++ book_management/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+from book_management.models import Category, Book
+
+# Create your views here.
+
+
+def index(request):
+    header_str = 'Hello, Python variable'
+    template = loader.get_template('index.html')
+    context = {
+        'var1': header_str,
+        'cat_num': len(Category.objects.all()),
+        'book_num': len(Book.objects.all()),
+
+    }
+    return render(request, 'index.html', context)

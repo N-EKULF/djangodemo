f8d418b5700244c66949fdbdf86782b7be7ba1e4
Natthaphol Aphaiso <natthaphol.dev@gmail.com>
2020-04-16 22:01:11 +0700

    no message

--- my_library/settings.py
+++ my_library/settings.py
@@ -47,11 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'my_library.urls'
 
